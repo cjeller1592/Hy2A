a5325fcf8703c8025121d8416d192d3ef025997c
CJ Eller <45696734+cjeller1592@users.noreply.github.com>
2019-05-31 16:58:20 -0400

    Take out main function for separate file

--- utils.py
+++ utils.py
@@ -79,37 +79,3 @@ def search(resource):
 			results.append(annotation)
 
 	return results
-
-# This combines everything together
-def main(resource):
-
-    channel = createChannel(resource)
-
-    annotations = search(resource)
-
-    for a in annotations:
-        text = a['text']
-        link = a['links']['incontext']
-        source = a['document']['title'][0]
-
-        for i in range(0, 4):
-            try:
-                quotedText = a['target'][0]['selector'][i]['exact']
-
-                if q != None:
-                    quotedText = q
-                    break
-
-            except Exception:
-                continue
-
-            quotedText = 'Error: could not find annotation'
-
-
-        body = '>%s\n\n-- "%s" ([source](%s))\n\n%s' % (quotedText, source, link, text)
-
-        block = createBlock(channel, body, title=None)
-
-# Will need to figure out what to return for real
-# Maybe the channel now updated w/ annotations?
-    return 'See if it worked!'
